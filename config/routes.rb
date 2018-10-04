14ad0d1719be571e0c55ff4c5cc038b4830e6a8a
Tyrant Davis <davistyrant@gmail.com>
2018-10-04 18:26:00 -0500

    Add tv route routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'tvs/index'
+  get 'tvs/show'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
