8918e714cb1397bd7bfe7f6a8533a803e471e771
Tyrant Davis <davistyrant@gmail.com>
2018-10-06 08:44:43 -0500

    Modify Welcome controller spec.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-  before(:all) do
-    @popular = Tv.get_popular_shows['results'].take(9)
-    @big_bang = Tv.get_popular_shows['results'][0]['original_name']
-  end
+    let (:popular) {Tv.get_popular_shows['results'].take(9)}
+    let(:big_bang) {Tv.get_popular_shows['results'][0]['original_name']}
+
 
   describe "GET #index" do
     it "returns http success" do
@@ -13,24 +12,23 @@ RSpec.describe WelcomeController, type: :controller do
     end
 
     it "returns an Array" do
-      expect(@popular).to be_a(Array)
-
+      expect(popular).to be_a(Array)
     end
 
     it "returns an Array that is not empty" do
-      expect(@popular).not_to be_empty
+      expect(popular).not_to be_empty
     end
 
     it "the Array - returns 9 shows " do
-      expect(@popular.count).to eql(9)
+      expect(popular.count).to eql(9)
     end
 
     it "returns show data as a Hash" do
-      expect(@popular[0]).to be_a(Hash)
+      expect(popular[0]).to be_a(Hash)
     end
 
     it "returns the The Big Bang Theory" do
-      expect(@big_bang).should eql?(/The Big Bang Theroy/)
+      expect(big_bang).to eql(/The Big Bang Theroy/)
     end
   end
 
