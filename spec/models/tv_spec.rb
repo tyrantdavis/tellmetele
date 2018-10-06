48a6a6e8ebbb5899db06a2a44b5bf3a4d6844eb7
Tyrant Davis <davistyrant@gmail.com>
2018-10-06 08:45:08 -0500

    Modify Tv model spec.

--- spec/models/tv_spec.rb
+++ spec/models/tv_spec.rb
@@ -1,13 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Tv, type: :model do
-  before(:all) do
-    @response = Tv.get_popular_shows
-  end
 
-  describe "#get_popular_shows" do
+    let(:tv_response) {Tv.get_popular_shows}
+
+
+  describe ".get_popular_shows" do
+    it "responds successfully" do
+      expect(response).to have_http_status(:success)
+    end
+
     it "returns a HTTParty response" do
-      expect(@response).to be_a(HTTParty::Response)
+      expect(tv_response).to be_a(HTTParty::Response)
+    end
+
+    it "returns data" do
+      expect(result).to be
     end
   end
 
