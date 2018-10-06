6c750c0bc1199d0dba68a7a03651147cedbdbc32
Tyrant Davis <davistyrant@gmail.com>
2018-10-06 03:27:59 -0500

    Add Inquriy connection.

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,12 @@
 class Inquiry < ApplicationRecord
+  include HTTParty
+
+  base_uri 'https://api.themoviedb.org/3'
+  default_params api_key: ENV['API_KEY']
+  format :json
+
+  def Inquiry.about(expression, page)
+    get("/search/multi", query: {query: expression, page: page})
+  end
+
 end
