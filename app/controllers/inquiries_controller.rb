07dba9b5578996b965898061dee1d8af7f23c625
Tyrant Davis <davistyrant@gmail.com>
2018-10-06 08:37:17 -0500

    Update Inquiry controller.

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,4 +1,31 @@
 class InquiriesController < ApplicationController
+
   def index
+    probe = params[:query]
+    pg = params[:page].to_i
+    session_inquiry = params[:inquiry]
+    session_inquiry || session_inquiry = probe
+
+    if !probe
+      probe,  pg = session_inquiry,  params[:inquiry_page]
+    else
+      session_inquiry,  session_inquiry = probe,  pg
+    end
+
+    pg = 1 if pg.eql?(0)
+    parsed_inquiry = Inquiry.about(probe, pg).parsed_response
+    pg_total = parsed_inquiry['total_pages']
+    results_total = parsed_inquiry['total_results']
+    inquiry_results = parsed_inquiry['results']
+
+    render :index, locals: {
+      probe: probe,
+      pg: pg,
+      session_inquiry: session_inquiry,
+      parsed_inquiry: parsed_inquiry,
+      pg_total: pg_total,
+      results_total: results_total,
+      inquiry_results: inquiry_results,
+    }
   end
 end
