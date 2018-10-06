dc25f8409ca674726fa027c36747ea4ad46a49cc
Tyrant Davis <davistyrant@gmail.com>
2018-10-06 08:39:39 -0500

    Modify Welcome controller vars.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,11 @@
 class WelcomeController < ApplicationController
   def index
     parsed_response = Tv.get_popular_shows.parsed_response
-    popular_shows = parsed_response['results'].take(9)
+    popular_shows = parsed_response['results'].drop(11)
     pages = parsed_response['total_pages']
     
     render :index, locals: {
+      parsed_response: parsed_response,
       popular_shows: popular_shows,
       pages: pages
     }
