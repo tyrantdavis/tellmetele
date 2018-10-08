b6d66eed09445144764d3877d65c5964087edc49
Tyrant Davis <davistyrant@gmail.com>
2018-10-08 04:38:00 -0500

    Add all shows and individula shows functionality to Tv model.

--- app/models/tv.rb
+++ app/models/tv.rb
@@ -5,8 +5,16 @@ class Tv < ApplicationRecord
   default_params api_key: ENV['API_KEY']
   format :json
 
+  def Tv.get_all_shows(page, sort)
+    get("/discover/tv", query: {sort_by: sort, page: page})
+  end
+
   def Tv.get_popular_shows
     get("/discover/tv", query: {sort_by: "popularity.desc"})
+  end
+
+  def Tv.get_single_show(tv_id)
+    get("/tv/#{tv_id}",query: {})
   end
 
 end
