dfffed7bc532a93dc98a25f78e7e42ad3772027a
Tyrant Davis <davistyrant@gmail.com>
2018-10-09 16:14:09 -0500

    Change the number of shows presented on homwpage.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     parsed_response = Tv.get_popular_shows.parsed_response
-    popular_shows = parsed_response['results'].drop(11)
+    popular_shows = parsed_response['results'].drop(8)
     pages = parsed_response['total_pages']
-    
+
     render :index, locals: {
       parsed_response: parsed_response,
       popular_shows: popular_shows,
